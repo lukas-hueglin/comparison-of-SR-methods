00d4be286f5099e2666d7c5f8da52b9a394512ef
lukas.hueglin <lukas.hueglin@outlook.com>
2022-03-18 18:05:25 +0100

    2
    Scrapped "setup.py" and "__init__.py" files and went with the simpler "requirements.txt" option.

--- setup.py
+++
@@ -1,9 +0,0 @@
-### setup.py ###
-from setuptools import setup, find_packages
-
-setup(
-    name='super-resolution-methods',
-    version='1.0.0',
-    packages=find_packages(include=['src']),
-    install_requires=[]
-)
