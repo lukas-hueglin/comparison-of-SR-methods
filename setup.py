594da467e0b3f12a18b2dacbeaf876409a53f7bd
lukas.hueglin <lukas.hueglin@outlook.com>
2022-03-18 17:41:11 +0100

    1
    Set up the project and added necessary files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+### setup.py ###
+from setuptools import setup, find_packages
+
+setup(
+    name='super-resolution-methods',
+    version='1.0.0',
+    packages=find_packages(include=['src']),
+    install_requires=[]
+)
